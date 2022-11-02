c8e533f0156340210e1172d84306c33c22e9b9d7
PINKTAIGAH <116960699+PINKTAIGAH@users.noreply.github.com>
2022-11-02 21:56:55 +0000

    >new file that contains all functions to create signals

---
+++ DAH_project/signal_generator.py
@@ -0,0 +1,72 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+def sine_wave(pitch, volume, duration):
+    
+    global OUTPUT_RATE, MAX_AMPLITUDE
+
+    total_samples= int(OUTPUT_RATE*duration)
+    t= np.linspace(0, duration, OUTPUT_RATE, endpoint=True)
+    amplitude= int(MAX_AMPLITUDE*volume)
+    
+    output_buffer= np.zeros( (total_samples, 2), dtype= np.int16)
+    signal_array= amplitude*signal.chirp(t, f0=pitch, t1= total_samples, f1= 3*pitch)
+    
+    for i in range(total_samples):
+        output_buffer[i][0]= output_buffer[i][1]= signal_array[i]
+        
+
+    return output_buffer
+
+def sawtooth_wave(pitch, volume, duration):
+    
+    global OUTPUT_RATE, MAX_AMPLITUDE
+
+    total_samples= int(OUTPUT_RATE*duration)
+    t= np.linspace(0, duration, OUTPUT_RATE, endpoint=True)
+    amplitude= int(MAX_AMPLITUDE*volume)
+    
+    output_buffer= np.zeros( (total_samples, 2), dtype= np.int16)
+    signal_array= amplitude*signal.sawtooth(2*np.pi*pitch*t)
+    
+    for i in range(total_samples):
+        output_buffer[i][0]= output_buffer[i][1]= signal_array[i]
+        
+
+    return output_buffer
+
+def square_wave(pitch, volume, duration):
+    
+    global OUTPUT_RATE, MAX_AMPLITUDE
+
+    total_samples= int(OUTPUT_RATE*duration)
+    t= np.linspace(0, duration, OUTPUT_RATE, endpoint=True)
+    amplitude= int(MAX_AMPLITUDE*volume)
+    
+    output_buffer= np.zeros( (total_samples, 2), dtype= np.int16)
+    signal_array= amplitude*signal.square(2*np.pi*pitch*t)
+    
+    for i in range(total_samples):
+        output_buffer[i][0]= output_buffer[i][1]= signal_array[i]
+        
+
+    return output_buffer
+
+def triangle_wave(pitch, volume, duration):
+    
+    global OUTPUT_RATE, MAX_AMPLITUDE
+
+    total_samples= int(OUTPUT_RATE*duration)
+    t= np.linspace(0, duration, OUTPUT_RATE, endpoint=True)
+    amplitude= int(MAX_AMPLITUDE*volume)
+    
+    output_buffer= np.zeros( (total_samples, 2), dtype= np.int16)
+    signal_array= amplitude*signal.sawtooth(2*np.pi*pitch*t, 0.5)
+    
+    for i in range(total_samples):
+        output_buffer[i][0]= output_buffer[i][1]= signal_array[i]
+        
+
+    return output_buffer
