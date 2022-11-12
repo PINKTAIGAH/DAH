9a44342d27da7f8cdb6f04b26262f3d333b5c965
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2022-11-12 22:38:27 +0000

    Added functionality to recognize multiple simultanious button inputs

--- DAH_project/button_tester.py
+++ DAH_project/button_tester.py
@@ -1,30 +1,25 @@
 from DAH import MCP23S17
+import numpy as np
+import time
 
 mcp= MCP23S17(chip= 0, address= 0x20)
-B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10= 0,1,2,3,4,5,6,7,8,9,10
+buttons= np.array(['B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7'], dtype= "str")
 
+def buttons_pressed(port_state):
+    index= []
+    for i, _ in enumerate(port_state):
+        
+        if port_state[i]== 0:
+            index.append(i)
+    return(index)
+    
 while True:
-    if mcp.digitalRead(B0)== False:
-        print(0)
-    elif mcp.digitalRead(B1)== False:
-        print(1)
-    elif mcp.digitalRead(B2)== False:
-        print(2)
-    elif mcp.digitalRead(B3)== False:
-        print(3)
-    elif mcp.digitalRead(B4)== False:
-        print(4)
-    elif mcp.digitalRead(B5)== False:
-        print(5)
-    elif mcp.digitalRead(B6)== False:
-        print(6)
-    elif mcp.digitalRead(B7)== False:
-        print(7)
-    elif mcp.digitalRead(B8)== False:
-        print(8)
-    elif mcp.digitalRead(B9)== False:
-        print(9)
-    elif mcp.digitalRead(B10)== False:
-        print(10)
-    else:
-        print('off')
+    port_state= bin(mcp.portRead())
+    port_state= [int(x) for x in str(port_state[2:])]
+    port_state= port_state[::-1]
+    down_state= buttons_pressed(port_state[:-2])
+    print(down_state)
+
+
+    
+    
